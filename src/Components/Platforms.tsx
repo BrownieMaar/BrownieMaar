42f5e2ecf19b01b4eb3c6670a4a16a0e354e7bd7
BrownieMaar <barnamarci07@gmail.com>
2023-03-01 09:29:42 +0100

    add platform page with links

--- src/Components/Platforms.tsx
+++ src/Components/Platforms.tsx
@@ -1,6 +1,22 @@
-export default function Platforms() {
+import { SiGithub, SiLinkedin } from "react-icons/si";
+import "./Platforms.css"
 
-    return <div className='card yellow' id='platforms'>
-    <h1>Here will be other platforms where you can check me out.</h1>
-  </div>
+export default function Platforms() {
+    const anchorizer = (link: string): void => {
+        window.open(link)
+    }
+
+    return <div className='card yellow platforms' id='platforms'>
+        <h1>I'm also available on these platforms:</h1>
+        <div className="links">
+            <div onClick={_e => anchorizer("https://www.linkedin.com/in/m%C3%A1rton-barna-43653b236/")}>
+                <SiLinkedin />
+                <span>LinkedIn</span>
+            </div>
+            <div onClick={_e => anchorizer("https://github.com/BrownieMaar")}>
+                <SiGithub />
+                <span>GitHub</span>
+            </div>
+        </div>
+    </div>
 }
