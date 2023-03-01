4d22c045edb102a2fc72d418cdb7a4f70c7d24c9
BrownieMaar <barnamarci07@gmail.com>
2023-03-01 06:48:02 +0100

    add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { SiCss3, SiHtml5, SiJava, SiJavascript, SiNodedotjs, SiReact, SiSpring } from "react-icons/si";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentSkill, setCurrentSkill] = useState("programming")
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App" id='App'>
+      <div className="hoverlog"><p>Hi.</p></div>
+      <div className='card skillpage'>
+        <div className="logos">
+          <div onMouseEnter={() => setCurrentSkill("HTML 5")} style={{color: "#e54c21"}}><SiHtml5 /></div>
+          <div onMouseEnter={() => setCurrentSkill("CSS")}style={{color: "#214ce5"}}><SiCss3 /></div>
+          <div onMouseEnter={() => setCurrentSkill("JavaScript")}style={{color: "#f7e018"}}><SiJavascript /></div>
+          <div onMouseEnter={() => setCurrentSkill("Node.js")}style={{color: "#7fc728"}}><SiNodedotjs /></div>
+          <div onMouseEnter={() => setCurrentSkill("React.js")}style={{color: "#5ed3f3"}}><SiReact /></div>
+          <div onMouseEnter={() => setCurrentSkill("Java")} style={{color: "#e51f24"}}><SiJava /></div>
+          <div onMouseEnter={() => setCurrentSkill("Spring")} style={{color: "#6aad3d"}}><SiSpring /></div>
+        </div>
+          <div style={{display: "block"}}>He understands <span style={{width: "10rem", display: "block"}}>{currentSkill}.</span></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='card red'>
+        <h1>foo</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='card'>
+        <h1>foo</h1>
+      </div>
+      <div className='card yellow'>
+        <h1>foo</h1>
+      </div>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+const meta = document.createElement('meta');
+meta.httpEquiv = 'Permissions-Policy';
+meta.content = "camera 'none'; microphone 'none'";
+document.head.appendChild(meta);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
